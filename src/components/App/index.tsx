7278b52b96f012829a0be235aa0a3a6584d7a9a2
Ruslan Orazliev <rorazliev@outlook.com>
2021-04-15 20:17:25 -0500

    Register routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, Suspense, useEffect } from 'react';
+import {
+  HashRouter as Router, Switch, Redirect, Route,
+} from 'react-router-dom';
 import ignoreRejection from '../../helpers/ignoreRejection';
 
 const App: React.FC = (): ReactElement => {
@@ -10,7 +13,17 @@ const App: React.FC = (): ReactElement => {
     };
   });
 
-  return <div />;
+  return (
+    <Router>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route exact path="/" render={() => null} />
+          <Route exact path="/:state" render={() => null} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
